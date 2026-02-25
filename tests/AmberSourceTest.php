f6da9585799c06d931d3653306e10fc6e23bc2fb
Daniel Schulz (Madeline) <faulolio@gmail.com>
2026-02-25 01:37:15 +0100

    test: amber

--- tests/AmberSourceTest.php
+++ tests/AmberSourceTest.php
@@ -57,9 +57,7 @@ final class AmberSourceTest extends TestCase {
         
         $versions = [
             '1.05' => 'Thalion-v1.05-DE',
-            '1.06' => 'Slothsoft-v1.06-DE',
-            '1.07' => 'Slothsoft-v1.07-DE',
-            '1.12' => 'Pyrdacor-v1.12-DE'
+            '1.06' => 'Slothsoft-v1.06-DE'
         ];
         
         $files = [
